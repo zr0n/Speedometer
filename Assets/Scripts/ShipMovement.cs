dd367b69bf94c534131eb9fa5db6e332f3a4b3ce
Luiz Fernando <fernandoaff1@gmail.com>
2019-10-20 10:47:15 -0300

    Initial Commit - Upload Files

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Speedometer
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class ShipMovement : MonoBehaviour
+    {
+        [SerializeField]
+        float acceleration = 1f;
+
+        [SerializeField]
+        float deceleration = 1f;
+
+        [SerializeField]
+        float rotationSpeed = 1f;
+
+        public float maxSpeed = 25f;
+
+        public Vector3 velocity {
+            get
+            {
+                return rb.velocity;
+            }
+        }
+
+        Rigidbody rb;
+
+        Vector3 lastPosition;
+        // Start is called before the first frame update
+        void Start()
+        {
+            lastPosition = transform.position;
+
+            rb = GetComponent<Rigidbody>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Steering(Input.GetAxis("Horizontal"), Input.GetAxis("Pitch"));
+
+            if (velocity.magnitude > maxSpeed)
+                NormalizeSpeed();
+        }
+        private void FixedUpdate()
+        {
+            Acceleration(Input.GetAxis("Vertical"));
+            
+
+            Debug.Log("Speed: " + velocity.magnitude);
+            lastPosition = transform.position;
+        }
+        void Steering(float horizontalAxis, float pitchAxis)
+        {
+            if (Mathf.Abs(horizontalAxis) > 0f || Mathf.Abs(pitchAxis) > 0f)
+                transform.Rotate(rotationSpeed * pitchAxis, rotationSpeed * horizontalAxis, 0, Space.Self);
+
+
+        }
+
+        void Acceleration(float verticalAxis)
+        {
+            if (Mathf.Abs(verticalAxis) == 0f)
+            {
+                Decelerate();
+                return;
+            }
+
+            rb.AddForce(transform.forward * acceleration * verticalAxis);
+        }
+
+        void NormalizeSpeed()
+        {
+            rb.velocity = velocity.normalized * maxSpeed;
+        }
+        
+        void Decelerate()
+        {
+            rb.AddForce(velocity * -1f * deceleration);
+        }
+    }
+}
---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+namespace Speedometer
+{
+    [RequireComponent(typeof(ProgressBarCircle))]
+    public class Speedometer : MonoBehaviour
+    {
+        [SerializeField]
+        ShipMovement ship;
+
+        [SerializeField]
+        string portName = "COM3";
+
+        SerialPort arduino;
+
+        float speedPerc;
+
+        ProgressBarCircle pb;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            pb = GetComponent<ProgressBarCircle>();
+            arduino = new SerialPort(portName, 9600);
+            arduino.Open();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!ship)
+                return;
+
+            speedPerc = (ship.velocity.magnitude / ship.maxSpeed) * 100f;
+
+            pb.BarValue = speedPerc;
+
+            if(speedPerc > 1f)
+            {
+                arduino.Write("1");
+
+                if (speedPerc > 25f)
+                {
+                    arduino.Write("2");
+                    if (speedPerc > 88f)
+                        arduino.Write("3");
+                    else
+                        arduino.Write("6");
+                        
+                }
+                else
+                {
+                    arduino.Write("5");
+                    arduino.Write("6");
+                }
+            }
+            else
+            {
+                arduino.Write("4");
+                arduino.Write("5");
+                arduino.Write("6");
+            }
+        }
+    }
+
+}
